41f8a48a8892fb096c145e4b2d9a3c3b319fff66
elmedinkarisik <edo.karisik1@gmail.com>
2020-10-23 00:12:57 +0200

    Initial commit

---
+++ pewdiepie.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://youtube.com')
+searchbox = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]/input')
+searchbox.send_keys('pewdiepie')
+
+searchButton = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/button')
+searchButton.click()
